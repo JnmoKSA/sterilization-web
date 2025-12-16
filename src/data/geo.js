88164024abde90c0e6b7a2a5cbe7a9194c3ea0a3
isddsgl <isddsgl@gmail.com>
2025-12-16 19:44:04 +0300

    Add GCC country/city dataset

---
+++ src/data/geo.js
@@ -0,0 +1,26 @@
+export const GCC_COUNTRIES = ["KSA", "UAE", "Qatar", "Kuwait", "Bahrain"];
+
+export const GCC_CITIES = {
+  KSA: [
+    "Riyadh","Jeddah","Makkah","Madinah","Dammam","Khobar","Dhahran","Taif",
+    "Abha","Khamis Mushait","Jazan","Najran","Buraidah","Unaizah","Hail",
+    "Tabuk","Al Khafji","Al Jubail","Yanbu","Al Ahsa (Hofuf)","Qatif","Arar",
+    "Sakaka","Al Bahah","Al Qunfudhah","Al Kharj"
+  ],
+  UAE: [
+    "Dubai","Abu Dhabi","Sharjah","Ajman","Ras Al Khaimah","Fujairah","Umm Al Quwain",
+    "Al Ain","Khor Fakkan","Dibba Al Fujairah","Kalba","Madinat Zayed","Ruwais"
+  ],
+  Qatar: [
+    "Doha","Al Rayyan","Al Wakrah","Al Khor","Umm Salal","Al Daayen","Lusail",
+    "Mesaieed","Dukhan","Al Shamal"
+  ],
+  Kuwait: [
+    "Kuwait City","Hawalli","Salmiya","Farwaniya","Ahmadi","Jahra","Mubarak Al-Kabeer",
+    "Sabah Al-Salem","Mangaf","Fahaheel","Abdali","Shuwaikh"
+  ],
+  Bahrain: [
+    "Manama","Muharraq","Riffa","Isa Town","Sitra","Hamad Town","Jidhafs",
+    "Budaiya","A&apos;ali","Saar","Busaiteen","Zallaq"
+  ],
+};
